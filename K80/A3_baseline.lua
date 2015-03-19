8cb9f426f577147e0b631e1b5582ccfeceda904c
Eduardo Franco <ef1265@nyu.edu>
2015-03-19 18:00:46 -0400

    Changed path on k80 files

--- K80/A3_baseline.lua
+++ K80/A3_baseline.lua
@@ -127,8 +127,8 @@ function main()
     -- Configuration parameters
     opt = {}
     -- change these to the appropriate data locations
-    opt.glovePath = "/home/ef1265/A3/glove/glove.6B.50d.txt" -- path to raw glove data .txt file
-    opt.dataPath = "/home/ef1265/A3/data/train.t7b"
+    opt.glovePath = "/home/eduardo/A3/glove/glove.6B.50d.txt" -- path to raw glove data .txt file
+    opt.dataPath = "/home/eduardo/A3/data/train.t7b"
     -- word vector dimensionality
     opt.inputDim = 50 
     -- nTrainDocs is the number of documents per class used in the training set, i.e.
