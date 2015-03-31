f3ac160d8fab3f14bb0ce97de06f4a46cdb60e6f
Eduardo Franco <ef1265@nyu.edu>
2015-03-31 11:47:00 -0400

    data quantization code added

---
+++ data_quantization.lua
@@ -0,0 +1,36 @@
+require 'torch';
+ffi = require 'ffi';
+require 'string';
+
+-- the dictionary of characters, 69
+dictionary = "abcdefghijklmnopqrstuvwxyz0123456789-,;.!?:'\"/\\|_@#$%^&*~`+-=<>()[]{}"
+train = torch.load("/scratch/courses/DSGA1008/A3/data/train.t7b")
+
+function quantization(document,frame,length)
+    document = document:lower()
+    x = torch.Tensor(frame,length):fill(0)
+    for i = 1,document:len() do
+        character = document:sub(i,i)
+        position = string.find(dictionary,character)
+        if position ~= nil then
+            x[position][i] = 1 
+        end
+    end
+    return x
+end
+
+function gettensor(i,j, input, frame, length)
+    index = train.index[i][j]
+    document = ffi.string(torch.data(train.content:narrow(1, index, 1)))
+    -- x is input tensor [frame x length]
+    x = quantization(input, frame, length)
+    return x
+end
+
+----------------------------------
+----------------------------------
+-- i=2
+-- j=100
+-- frame = dictionary:len()
+-- length = 256
+-- x = get_tensor(i,j)
