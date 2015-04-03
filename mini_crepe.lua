35e376e8c574bb6b6c9f6816bd2b4818d2e6737c
Eduardo Franco <ef1265@1pp-1.cusp.nyu.edu>
2015-04-03 13:56:51 -0400

    modified models

--- mini_crepe.lua
+++ mini_crepe.lua
@@ -51,107 +51,7 @@ function preprocess_data(raw_data, opt, dictionary)
 end
 
 
-function train_model(model, criterion, training_data, training_labels, opt)
-
-	-- classes
-	classes = {'1','2','3','4','5'}
-
-	-- This matrix records the current confusion across classes
-	confusion = optim.ConfusionMatrix(classes)
-
-    parameters,gradParameters = model:getParameters()
-
-    -- configure optimizer
-    optimState = {
-    	learningRate = opt.learningRate,
-    	weightDecay = opt.weightDecay,
-    	momentum = opt.momentum,
-    	learningRateDecay = opt.learningRateDecay
-    }
-    optimMethod = optim.sgd
-
-    epoch = epoch or 1
-	local time = sys.clock()
-
-	model:training()
-
-	inputs = torch.zeros(opt.batchSize,opt.frame,opt.length):cuda()
-	targets = torch.zeros(opt.batchSize):cuda()
-
-	-- do one epoch
-	print("\n==> online epoch # " .. epoch .. ' [batchSize = ' .. opt.batchSize .. ']')
-	for t = 1,training_data:size(1),opt.batchSize do
-		-- disp progress
-		-- xlua.progress(t, training_data:size(1))
-		inputs:zero()
-		targets:zero()
-
-		-- create mini batch
-		if t + opt.batchSize-1 <= training_data:size(1) then
-			-- xx = opt.batchSize
-			inputs[{}] = training_data[{ {t,t+opt.batchSize-1},{},{} }]
-			targets[{}] = training_labels[{ {t,t+opt.batchSize-1} }]
-		
-			-- create closure to evaluate f(X) and df/dX
-			local feval = function(x)
-				-- get new parameters
-				if x ~= parameters then
-					parameters:copy(x)
-				end
-				-- reset gradients
-				gradParameters:zero()
-				-- f is the average of all criterions
-				local f = 0
-				-- evaluate function for complete mini batch
-				-- estimate f
-				local output = model:forward(inputs:transpose(2,3))
-				local err = criterion:forward(output, targets)
-				f = f + err
-				-- estimate df/dW
-				local df_do = criterion:backward(output, targets)
-				model:backward(inputs:transpose(2,3), df_do)
-				-- update confusion
-				for k=1,opt.batchSize do
-					confusion:add(output[k], targets[k])
-				end
-				-- return f and df/dX
-				return f,gradParameters
-			end
-
-			-- optimize on current mini-batch
-			optimMethod(feval, parameters, optimState)
-		end
-	end
-
-	-- time taken
-	time = sys.clock() - time
-	time = time / training_data:size(1)
-	print("==> time to learn 1 sample = " .. (time*1000) .. 'ms')
-
-	confusion:updateValids()
-
-	-- print accuracy
-	print("==> training accuracy for epoch " .. epoch .. ':')
-	-- print(confusion)
-	print(confusion.totalValid*100)
-
-	-- save/log current net
-	local filename = paths.concat(opt.save, 'model.net')
-	os.execute('mkdir -p ' .. sys.dirname(filename))
-	print('==> saving model to '..filename)
-	torch.save(filename, model)
-
-	-- next epoch
-	confusion:zero()
-	epoch = epoch + 1
-
-end
-
--- training function as it appears in the baseline code
--- function train_model(model, criterion, data, labels, test_data, test_labels, opt)
-
--- 	model:cuda()
--- 	criterion:cuda()
+-- function train_model(model, criterion, training_data, training_labels, opt)
 
 -- 	-- classes
 -- 	classes = {'1','2','3','4','5'}
@@ -159,42 +59,139 @@ end
 -- 	-- This matrix records the current confusion across classes
 -- 	confusion = optim.ConfusionMatrix(classes)
 
---     parameters, grad_parameters = model:getParameters()
+--     parameters,gradParameters = model:getParameters()
 
---     minibatch = torch.zeros(opt.batchSize, opt.frame, opt.length):cuda()
---     minibatch_labels = torch.zeros(opt.batchSize):cuda()
-    
---     -- optimization functional to train the model with torch's optim library
---     local function feval(x) 
-        
---         minibatch:zero()
---         minibatch_labels:zero()
---         minibatch[{}] = data[{ {opt.idx,opt.idx+opt.batchSize-1},{},{} }]
---         minibatch_labels[{}] = labels[{ {opt.idx,opt.idx+opt.batchSize-1} }]
-        
---         model:training()
---         local minibatch_loss = criterion:forward(model:forward(minibatch:transpose(2,3):contiguous()), minibatch_labels)
---         model:zeroGradParameters()
---         model:backward(minibatch:transpose(2,3):contiguous(), criterion:backward(model.output, minibatch_labels))
-        
---         return minibatch_loss, grad_parameters
---     end
-    
---     for epoch=1,opt.nEpochs do
---         local order = torch.randperm(opt.nBatches) -- not really good randomization
---         for batch=1,opt.nBatches do
---             opt.idx = (order[batch] - 1) * opt.batchSize + 1
---             optim.sgd(feval, parameters, opt)
---             -- print("epoch: ", epoch, " batch: ", batch)
---         end
+--     -- configure optimizer
+--     optimState = {
+--     	learningRate = opt.learningRate,
+--     	weightDecay = opt.weightDecay,
+--     	momentum = opt.momentum,
+--     	learningRateDecay = opt.learningRateDecay
+--     }
+--     optimMethod = optim.sgd
 
---         print("==> testing on test set for epoch " .. epoch .. "")
---         test_model(model, test_data, test_labels, opt)
---         -- local accuracy = test_model(model, test_data, test_labels, opt)
---         -- print("epoch ", epoch, " error: ", accuracy)
+--     epoch = epoch or 1
+-- 	local time = sys.clock()
 
---     end
+-- 	model:training()
+
+-- 	inputs = torch.zeros(opt.batchSize,opt.frame,opt.length):cuda()
+-- 	targets = torch.zeros(opt.batchSize):cuda()
+
+-- 	-- do one epoch
+-- 	print("\n==> online epoch # " .. epoch .. ' [batchSize = ' .. opt.batchSize .. ']')
+-- 	for t = 1,training_data:size(1),opt.batchSize do
+-- 		-- disp progress
+-- 		-- xlua.progress(t, training_data:size(1))
+-- 		inputs:zero()
+-- 		targets:zero()
+
+-- 		-- create mini batch
+-- 		if t + opt.batchSize-1 <= training_data:size(1) then
+-- 			-- xx = opt.batchSize
+-- 			inputs[{}] = training_data[{ {t,t+opt.batchSize-1},{},{} }]
+-- 			targets[{}] = training_labels[{ {t,t+opt.batchSize-1} }]
+		
+-- 			-- create closure to evaluate f(X) and df/dX
+-- 			local feval = function(x)
+-- 				-- get new parameters
+-- 				if x ~= parameters then
+-- 					parameters:copy(x)
+-- 				end
+-- 				-- reset gradients
+-- 				gradParameters:zero()
+-- 				-- f is the average of all criterions
+-- 				local f = 0
+-- 				-- evaluate function for complete mini batch
+-- 				-- estimate f
+-- 				local output = model:forward(inputs:transpose(2,3))
+-- 				local err = criterion:forward(output, targets)
+-- 				f = f + err
+-- 				-- estimate df/dW
+-- 				local df_do = criterion:backward(output, targets)
+-- 				model:backward(inputs:transpose(2,3), df_do)
+-- 				-- update confusion
+-- 				for k=1,opt.batchSize do
+-- 					confusion:add(output[k], targets[k])
+-- 				end
+-- 				-- return f and df/dX
+-- 				return f,gradParameters
+-- 			end
+
+-- 			-- optimize on current mini-batch
+-- 			optimMethod(feval, parameters, optimState)
+-- 		end
+-- 	end
+
+-- 	-- time taken
+-- 	time = sys.clock() - time
+-- 	time = time / training_data:size(1)
+-- 	print("==> time to learn 1 sample = " .. (time*1000) .. 'ms')
+
+-- 	confusion:updateValids()
+
+-- 	-- print accuracy
+-- 	print("==> training accuracy for epoch " .. epoch .. ':')
+-- 	-- print(confusion)
+-- 	print(confusion.totalValid*100)
+
+-- 	-- save/log current net
+-- 	local filename = paths.concat(opt.save, 'model.net')
+-- 	os.execute('mkdir -p ' .. sys.dirname(filename))
+-- 	print('==> saving model to '..filename)
+-- 	torch.save(filename, model)
+
+-- 	-- next epoch
+-- 	confusion:zero()
+-- 	epoch = epoch + 1
+
 -- end
+
+-- training function as it appears in the baseline code
+function train_model(model, criterion, data, labels, test_data, test_labels, opt)
+
+	-- classes
+	classes = {'1','2','3','4','5'}
+
+	-- This matrix records the current confusion across classes
+	confusion = optim.ConfusionMatrix(classes)
+
+    parameters, grad_parameters = model:getParameters()
+
+    minibatch = torch.zeros(opt.batchSize, opt.frame, opt.length):cuda()
+    minibatch_labels = torch.zeros(opt.batchSize):cuda()
+    
+    -- optimization functional to train the model with torch's optim library
+    local function feval(x) 
+        
+        minibatch:zero()
+        minibatch_labels:zero()
+        minibatch[{}] = data[{ {opt.idx,opt.idx+opt.batchSize-1},{},{} }]
+        minibatch_labels[{}] = labels[{ {opt.idx,opt.idx+opt.batchSize-1} }]
+        
+        model:training()
+        local minibatch_loss = criterion:forward(model:forward(minibatch:transpose(2,3):contiguous()), minibatch_labels)
+        model:zeroGradParameters()
+        model:backward(minibatch:transpose(2,3):contiguous(), criterion:backward(model.output, minibatch_labels))
+        
+        return minibatch_loss, grad_parameters
+    end
+    
+    for epoch=1,opt.nEpochs do
+        local order = torch.randperm(opt.nBatches) -- not really good randomization
+        for batch=1,opt.nBatches do
+            opt.idx = (order[batch] - 1) * opt.batchSize + 1
+            optim.sgd(feval, parameters, opt)
+            -- print("epoch: ", epoch, " batch: ", batch)
+        end
+
+        print("==> testing on test set for epoch " .. epoch .. "")
+        test_model(model, test_data, test_labels, opt)
+        -- local accuracy = test_model(model, test_data, test_labels, opt)
+        -- print("epoch ", epoch, " error: ", accuracy)
+
+    end
+end
 
 function test_model(model, data, labels, opt)
 
@@ -327,14 +324,14 @@ function main()
 	model:cuda()
 	criterion:cuda()
 
-	-- train_model(model, criterion, training_data, training_labels, test_data, test_labels, opt)
+	train_model(model, criterion, training_data, training_labels, test_data, test_labels, opt)
     -- local results = test_model(model, test_data, test_labels)
     -- print(results)
-	print("\nTraining model...")
-    for i=1,opt.nEpochs do
-		train_model(model, criterion, training_data, training_labels, opt)
-        test_model(model,test_data,test_labels,opt)
-	end
+	-- print("\nTraining model...")
+ --    for i=1,opt.nEpochs do
+	-- 	train_model(model, criterion, training_data, training_labels, opt)
+ --        test_model(model,test_data,test_labels,opt)
+	-- end
     
 end
 
