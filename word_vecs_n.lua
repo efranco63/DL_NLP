c8a63e6ce1dbca7ed1985572dbccf781f875681e
Eduardo Franco <ef1265@nyu.edu>
2015-04-08 19:26:31 -0400

    processing and training data in 5 batches

--- word_vecs_n.lua
+++ word_vecs_n.lua
@@ -303,8 +303,8 @@ function main()
     model:add(nn.TemporalMaxPooling(3,3))
 
     -- 1st fully connected layer (19x512)
-    model:add(nn.Reshape(30*512))
-    model:add(nn.Linear(30*512,1024))
+    model:add(nn.Reshape(13*512))
+    model:add(nn.Linear(13*512,1024))
     model:add(nn.Threshold())
     model:add(nn.Dropout(0.7))
 
