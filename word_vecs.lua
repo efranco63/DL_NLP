5092d406a1bbc5c3f6aed1b31f9c3f1a91d168e9
Eduardo Franco <ef1265@nyu.edu>
2015-04-08 08:16:10 -0400

    2 layer, 300 input dim, 200 length, 512 width, seed 8, learn rate 0.01 - anneal learn rate by 10 when accuracy drops, second layer max pooling 3, batch size = 64

--- word_vecs.lua
+++ word_vecs.lua
@@ -274,8 +274,8 @@ function main()
     model:add(nn.TemporalMaxPooling(3,3))
 
     -- 1st fully connected layer (19x512)
-    model:add(nn.Reshape(47*512))
-    model:add(nn.Linear(47*512,1024))
+    model:add(nn.Reshape(30*512))
+    model:add(nn.Linear(30*512,1024))
     model:add(nn.Threshold())
     model:add(nn.Dropout(0.7))
 
