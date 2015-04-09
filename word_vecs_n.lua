c04dbadce9cad0417d01b16fd993da679091c3d3
Eduardo Franco <ef1265@nyu.edu>
2015-04-09 19:02:51 -0400

    2 layer, 300 input dim, 200 length, 512 width, seed 8, learn rate 0.01 - anneal learn rate by 10 when accuracy drops, second layer max pooling 3, batch size = 64, dropout = 0.7, random weight initialization

--- word_vecs_n.lua
+++ word_vecs_n.lua
@@ -209,7 +209,7 @@ function test_model(model, data, labels, opt)
 
     -- save/log current net
     if accuracy > accs['max'] then 
-        local filename = paths.concat(opt.save, 'model12.net')
+        local filename = paths.concat(opt.save, 'model12_2.net')
         os.execute('mkdir -p ' .. sys.dirname(filename))
         print('==> saving model to '..filename)
         torch.save(filename, model)
